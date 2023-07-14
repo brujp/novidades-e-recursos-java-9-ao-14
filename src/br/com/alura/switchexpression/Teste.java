638dc1622a90537752a8f0f498770f681f3e2731
Bruno Pinto <bruno.jpinto@outlook.com>
2023-07-13 23:12:58 -0300

    Trabalhando com blocos de texto

--- src/br/com/alura/switchexpression/Teste.java
+++ src/br/com/alura/switchexpression/Teste.java
@@ -3,13 +3,13 @@ package br.com.alura.switchexpression;
 public class Teste {
 
 	public static void main(String[] args) {
-		String nome = "Jo„o";
+		String nome = "Jo√£o";
 		switch (nome) {
 		case "Renata": {
 			System.out.println("Acertou: " + nome);
 			break;
 		}
-		case "Jo„o": {
+		case "Jo√£o": {
 			System.out.println("Acertou: " + nome);
 			break;
 		}
