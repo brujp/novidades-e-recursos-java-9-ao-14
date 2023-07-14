4124af679e425b51396e277894f902b03f6ae029
Bruno Pinto <bruno.jpinto@outlook.com>
2023-07-13 23:08:10 -0300

    Inferencia de variáveis

--- src/br/com/alura/inferenciavariavel/Teste.java
+++ src/br/com/alura/inferenciavariavel/Teste.java
@@ -5,9 +5,13 @@ import java.util.HashMap;
 public class Teste {
 
 	public static void main(String[] args) {
-		HashMap<String, String> cpfPorNomes = new HashMap<String, String>();
-		cpfPorNomes.put("Jo„o", "04813189");
 
+		HashMap<String, String> cpfPorNomes = new HashMap<>();
+		cpfPorNomes.put("Jo√£o", "04813189");
 		System.out.println(cpfPorNomes);
+
+		var cpfPorNomesDois = new HashMap<String, String>();
+		cpfPorNomesDois.put("Bruno", "4625253");
+		System.out.println(cpfPorNomesDois);
 	}
 }
